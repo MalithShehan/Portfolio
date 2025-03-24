2e01638b9fb8863d7408113c0a61edaf7c154d79
ASUS <shehansandakalum2003@gmail.com>
2025-03-24 10:20:46 +0530

    App jsx edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import {Component} from "react";
 import Navbar from "./components/NavBar.jsx";
 import Hero from "./components/Hero.jsx";
+import Skills from "./components/Skills.jsx";
 
 export default function App(){
     return (
         <>
             <Navbar />
             <Hero />
+            <Skills />
         </>
     )
 }
