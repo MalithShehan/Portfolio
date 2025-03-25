ab4f24f0a1b3c30b04414a5d74759219f4e00a6b
ASUS <shehansandakalum2003@gmail.com>
2025-03-25 22:53:09 +0530

    Skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {DiCss3, DiGithub, DiHtml5, DiJavascript1, DiMongodb, DiMysql, DiNodejsSmall, DiReact} from "react-icons/di";
+import Reveal from "./Reveal.jsx";
 
 const skills = [
     {
@@ -26,13 +27,14 @@ const skills = [
 ]
 const Skills = () => {
     return (
-        <div className="flex flex-col justify-center px-4 text-gray-200 pb-8 md:py-12" id="skills">
+        <div className="max-w-[650px] mx-auto flex flex-col justify-center px-4 text-gray-200 pb-8 md:py-12 items-center" id="skills">
+            <Reveal>
             <h2 className="text-3xl font-bold mb-4 text-center">Skills</h2>
             <p className="text-center mb-8 ">
                 I worked on various frontend and backend projects. Check them <a href="#" className="underline">there</a>
             </p>
 
-            <div className="flex flex-col md:flex-row justify-center space-y-8 md:space-y-0 md:space-x-8 max-w-[1000px] mx-auto">
+            <div className="flex flex-col md:flex-row justify-center space-y-8 md:space-y-0 md:space-x-8 ">
                 {skills.map((skill, index) => (
                     <div key={index} className="border border-purple-900 p-6 rounded-lg bg-purple-900/20 shadow-lg w-full md:w-1/2">
                         <h3 className="text-xl font-bold mb-4 text-center">{skill.category}</h3>
@@ -47,6 +49,7 @@ const Skills = () => {
                     </div>
                 ))}
             </div>
+            </Reveal>
         </div>
     )
 }
